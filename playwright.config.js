fddece571f9b9465f442aaf6886d19e19787f99b
saravijr10 <saravanan.duraiswami10@gmail.com>
2026-03-30 14:01:37 +0530

    windows + cross browser pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from '@playwright/test';
 
-module.exports = defineConfig({
+export default defineConfig({
     testDir: './tests',
     reporter: [['list']],
+
+    projects: [
+        {
+            name: 'chromium',
+            use: { browserName: 'chromium' },
+        },
+        {
+            name: 'firefox',
+            use: { browserName: 'firefox' },
+        },
+        {
+            name: 'webkit',
+            use: { browserName: 'webkit' },
+        },
+    ],
 });
